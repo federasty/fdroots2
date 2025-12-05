8e8ac8ff6f16bcc33db6c087116d266ef3890252
federasty <federicodaniel7@gmail.com>
2025-12-05 17:16:04 -0300

    ultimo fix de navbar y hero

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Hero from "./components/Hero";
 
 
 
+
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <NavBar />
        <Hero />
+   
 
     </div>
   );
